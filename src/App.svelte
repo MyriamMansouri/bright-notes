199edca6d34bb56ed60a6b3626ddf70538dc9ce3
Myriam Mansouri <myriam.mansouri@radio-canada.ca>
2024-08-30 17:02:12 -0400

    chore: tailwind config

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,8 @@
     export let name;
 </script>
 
-<main>
-    <h1 class="text-4xl text-blue-700">{name}'s app</h1>
+<main class="bg-orange-500 h-40 w-40 flex flex-row">
+    <h1 class="text-2xl w-10 text-green-700 underline decoration-slate-400 block justify-around">{name}'s app</h1>
 </main>
 <style>
   /* styles go here */
