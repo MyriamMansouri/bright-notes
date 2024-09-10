d40ba6f2aec9e06c8c7562bd651efdb06afa796f
Myriam Mansouri <myriam.mansouri@radio-canada.ca>
2024-09-09 22:29:09 -0400

    chore: packages + svelte config update

---
+++ svelte.config.mjs
@@ -0,0 +1,10 @@
+import { sveltePreprocess } from 'svelte-preprocess';
+
+const config = {
+  preprocess: sveltePreprocess({
+    postcss: true,
+    typescript: true,
+  }),
+};
+
+export default config;
